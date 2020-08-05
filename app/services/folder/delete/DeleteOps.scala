89411b0cfb6a02bcfc59ba9f36484a52dfa122b7
Zongwen Fan <zongwen.fan@uon.edu.au>
2020-08-05 19:15:44 +1000

    Delete associated documents and relationships with other tables (sharing_policy, document_filepart, document, folder_association)

--- app/services/folder/delete/DeleteOps.scala
+++ app/services/folder/delete/DeleteOps.scala
@@ -3,14 +3,32 @@ package services.folder.delete
 import java.util.UUID
 import scala.concurrent.Future
 import services.folder.FolderService
-import services.generated.Tables.{FOLDER, FOLDER_ASSOCIATION, SHARING_POLICY}
+import services.generated.Tables.{DOCUMENT_FILEPART,DOCUMENT,FOLDER, FOLDER_ASSOCIATION, SHARING_POLICY}
 
 trait DeleteOps { self: FolderService => 
 
-  def deleteFolder(id: UUID): Future[Boolean] = 
+  def deleteFolder(id: UUID): Future[Boolean] = {
     db.withTransaction { sql => 
+      // Delete sharing policies
+      sql.deleteFrom(SHARING_POLICY)
+         .where(SHARING_POLICY.DOCUMENT_ID.in(sql.select(FOLDER_ASSOCIATION.DOCUMENT_ID).from(FOLDER_ASSOCIATION)
+          .where(FOLDER_ASSOCIATION.FOLDER_ID.equal(id))))
+         .execute()
+      // Delete filepart records
+      sql.deleteFrom(DOCUMENT_FILEPART)
+         .where(DOCUMENT_FILEPART.DOCUMENT_ID.in(sql.select(FOLDER_ASSOCIATION.DOCUMENT_ID).from(FOLDER_ASSOCIATION)
+          .where(FOLDER_ASSOCIATION.FOLDER_ID.equal(id))))
+         .execute()
+      sql.deleteFrom(DOCUMENT)
+         .where(DOCUMENT.ID.in(sql.select(FOLDER_ASSOCIATION.DOCUMENT_ID).from(FOLDER_ASSOCIATION)
+          .where(FOLDER_ASSOCIATION.FOLDER_ID.equal(id))))
+         .execute()
+      sql.deleteFrom(FOLDER_ASSOCIATION)
+         .where(FOLDER_ASSOCIATION.FOLDER_ID.equal(id))
+         .execute
       sql.deleteFrom(FOLDER).where(FOLDER.ID.equal(id)).execute == 1
     }
+  }
 
   def deleteReadme(id: UUID): Future[Boolean] =
     db.withTransaction { sql =>
